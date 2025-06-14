9965b6e4879413fc3fe1185e72d6c6230bfffa3e
afiksahar <afikoo52@gmail.com>
2025-06-14 20:27:38 +0300

    הוספת db.create_all() ליצירת טבלאות בשרת

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///appointments.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Client(db.Model):
@@ -14,13 +16,15 @@ class Client(db.Model):
 class Appointment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     client_id = db.Column(db.Integer, db.ForeignKey('client.id'), nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+    time = db.Column(db.String(50), nullable=False)
     treatment = db.Column(db.String(100), nullable=False)
+
     client = db.relationship('Client', backref=db.backref('appointments', lazy=True))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('calendar.html')
 
 @app.route('/add-client', methods=['GET', 'POST'])
 def add_client():
@@ -30,42 +34,39 @@ def add_client():
         new_client = Client(name=name, phone=phone)
         db.session.add(new_client)
         db.session.commit()
-        return redirect('/')
+        return redirect('/clients')
     return render_template('add_client.html')
+
+@app.route('/clients')
+def clients():
+    all_clients = Client.query.all()
+    return render_template('clients.html', clients=all_clients)
 
 @app.route('/add-appointment', methods=['GET', 'POST'])
 def add_appointment():
     clients = Client.query.all()
     if request.method == 'POST':
         client_id = request.form['client_id']
-        date_str = request.form['date']
+        date = request.form['date']
+        time = request.form['time']
         treatment = request.form['treatment']
-        date = datetime.strptime(date_str, '%Y-%m-%dT%H:%M')
-        new_appointment = Appointment(client_id=client_id, date=date, treatment=treatment)
+        new_appointment = Appointment(client_id=client_id, date=date, time=time, treatment=treatment)
         db.session.add(new_appointment)
         db.session.commit()
         return redirect('/')
     return render_template('add_appointment.html', clients=clients)
 
-@app.route('/clients')
-def view_clients():
-    clients = Client.query.all()
-    return render_template('clients.html', clients=clients)
-
-@app.route('/calendar')
-def calendar():
-    return render_template('calendar.html')
-
 @app.route('/appointments-json')
 def appointments_json():
     appointments = Appointment.query.all()
-    events = []
-    for appt in appointments:
-        events.append({
-            'title': f"{appt.client.name} - {appt.treatment}",
-            'start': appt.date.isoformat()
+    data = []
+    for a in appointments:
+        client = Client.query.get(a.client_id)
+        data.append({
+            "title": f"{client.name} - {a.treatment}",
+            "start": f"{a.date}T{a.time}"
         })
-    return jsonify(events)
+    return jsonify(data)
 
 if __name__ == '__main__':
     with app.app_context():
